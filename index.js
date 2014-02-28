2ad241d911afc6f023bf5945d9a626c96d0af618
gagoar <xeroice@gmail.com>
2014-02-28 01:17:26 -0300

    if the image is cached???

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var BlurMath  = require('./src/BlurMath')
   , images    = document.querySelectorAll('img[data-blurit]')
-  , callback  = function (e) {
+  , blank     = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="
 
-    var image    =  this
-    , canvasId = image.getAttribute('data-blurit-canvas')
-    , bluredId = image.getAttribute('data-blurit-onimg')
-    , callback = image.getAttribute('data-blurit-callabck')
-    , action   = image.getAttribute('data-blurit-transform')
-    , radius   = image.getAttribute('data-blurit-radius') || 20
-    , canvas   = canvasId ? document.getElementById(canvasId) : document.createElement('canvas')
-    , blured   = bluredId ? document.getElementById(bluredId) : document.createElement('img')
+  , handelEvent = function (e) {
+    process(this)
+  }
+  , process  = function (image) {
+
+    var canvasId = image.getAttribute('data-blurit-canvas')
+      , bluredId = image.getAttribute('data-blurit-onimg')
+      , callback = image.getAttribute('data-blurit-callabck')
+      , action   = image.getAttribute('data-blurit-transform')
+      , radius   = image.getAttribute('data-blurit-radius') || 20
+      , canvas   = canvasId ? document.getElementById(canvasId) : document.createElement('canvas')
+      , blured   = bluredId ? document.getElementById(bluredId) : document.createElement('img')
 
     if (canvas) {
       var dataURL = BlurMath.process(image, canvas, Number(radius) , false)
@@ -31,5 +35,9 @@ var BlurMath  = require('./src/BlurMath')
 
   }
 
-
-  for (var i = 0, img; img = images[i++];) img.onload = callback
+  for (var i = 0, img; img = images[i++];) {
+    if (img.complete)
+      process(img)
+    else
+      img.onload = callback
+  }
